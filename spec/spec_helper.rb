97f9ed17562e25e611937b91fca76f89ee1197d0
tim.sheng <tim.sheng@overseasstudentliving.com>
2016-08-30 11:23:31 +0800

    remove parameters in initialize method

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,12 @@ require 'rspec'
 require './api/wechat'
 require './payloads/wechat_payload'
 require 'cleric'
+
+# 
+# def mock_ssh
+#   ssh = double('wechat_stage')
+# end
+#
+# def mock_db
+#   db = double('wechat_db')
+# end
