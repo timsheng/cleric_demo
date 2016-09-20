fd43adf3a6d22baad1f5f6c7b0fe871b6908a0f2
dan-pan <382924682@qq.com>
2016-09-20 15:44:17 +0800

    encrypt and decrypt script

--- bin/decrypt.rb
+++ bin/decrypt.rb
@@ -1,11 +1,12 @@
 require 'rbnacl'
 require 'base64'
 
-file = "../config/environments/stage.yml"
+file = "../config/environments/stage.yml.new"
 file_enc = "../config/environments/stage.yml.enc"
+
 key_env = ENV['KEY_ENV']
 key = Base64.decode64(key_env)
 box = RbNaCl::SimpleBox.from_secret_key(key)
-plaintext = File.read(file)
-ciphertext = box.encrypt(plaintext)
-File.write(file_enc, ciphertext)
+ciphertext = File.read(file_enc)
+plaintext = box.decrypt(ciphertext)
+File.write(file, plaintext)
