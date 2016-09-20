c1171cbbc7ae5f2ec9869626d774df5750dd0932
dan-pan <382924682@qq.com>
2016-09-20 16:29:32 +0800

    remove shell script

--- sh/decrypt.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-path=../config/environments/
-openssl enc -in  $path/stage.yml.enc -out  $path/stage.yml.new -d -aes256 -k $1
--- sh/encrypt.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-path=../config/environments/
-openssl enc -in  $path/stage.yml -out  $path/stage.yml.enc -e -aes256 -k $1
