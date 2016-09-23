98b20d15fdc24cef32ab51fffb4592adb31365ce
dan-pan <382924682@qq.com>
2016-09-23 10:49:47 +0800

    user wget instead of curl

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 
 # Run confd to generate the application configurations
 echo "Generating configurations from /app/config with prefix $ENV_CONFD_PREFIX"
-#confd -onetime -backend env -confdir /app/config -prefix=$ENV_CONFD_PREFIX
-#sed -i 's/\\n/\n/g' /app/config/id_rsa
-#mkdir ~/.ssh && cp /app/config/id_rsa ~/.ssh
+confd -onetime -backend env -confdir /app/config -prefix=$ENV_CONFD_PREFIX
+sed -i 's/\\n/\n/g' /app/config/id_rsa
+mkdir ~/.ssh && cp /app/config/id_rsa ~/.ssh
 
 # Decrypt yml
 echo "Decrypt environment yml"
