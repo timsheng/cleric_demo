436639eab91e5f4876c7b4784601d5b4c79e195d
tim.sheng <tim.sheng@overseasstudentliving.com>
2016-09-27 23:22:04 +0800

    add db query method to accessor.rb file, rewrite prep_data again for data_magic ,give api class dynamic generate sql function

--- api/frontend_facade.rb
+++ api/frontend_facade.rb
@@ -7,8 +7,20 @@ class FrontendFacade < API
 
   base_uri http('base_uri')
 
+  query :id, :db => 'properties',:name => '1 Penta House'
+  query :name, :db => 'properties', :id => 1
+  query :address, :db => 'properties', :id => 1
+
   def create_user payload
     self.class.post('/users', :body => payload.to_json)
   end
+
+  def expect_result key
+    DataMagic.load 'frontendfacade.yml'
+    data_for(key)['response']
+  end
+
+
+
 
 end
