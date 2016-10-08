48ba0ba05e1853978a81e5e7436c274ca25cbb6d
dan-pan <382924682@qq.com>
2016-10-08 12:33:17 +0800

    use te-puni-village instead of student-village for zh-cn

--- spec/api/frontend_facade_spec.rb
+++ spec/api/frontend_facade_spec.rb
@@ -15,8 +15,8 @@ describe "Frontend Facade" do
   end
 
   describe 'Property' do
-    context "Property summary" do
 
+    context "Property summary" do
       it "get summary for a property for en-gb", :tag => 'student_villiage_summary_en' do |example|
         key = example.metadata[:tag]
         frontend_facade_payload = FrontendFacadePayload.new(key)
@@ -27,24 +27,26 @@ describe "Frontend Facade" do
         expect(result).to eq(expected)
       end
 
-      it "get summary for a property for zh-cn", :tag => 'student_villiage_summary_cn' do |example|
+      it "get summary for a property for zh-cn", :tag => 'te_puni_village_summary_cn' do |example|
         key = example.metadata[:tag]
         frontend_facade_payload = FrontendFacadePayload.new(key)
         expected = frontend_facade_payload.payload
-        response = frontend_facade.get_summary_for_a_property('student-village','zh-cn')
+        response = frontend_facade.get_summary_for_a_property('te-puni-village','zh-cn')
         result = response.parsed_response
         expect(response.code).to be(200)
         expect(result).to eq(expected)
       end
+    end
 
-    context "Get Property" do
-      it "dbfactory demo" ,:tag => 'Users2' do |example|
-        key = example.metadata[:tag]
-        frontend_facade = FrontendFacade.new(:ssh => 'Property_ssh', :db => 'Property_db')
-        # puts frontend_facade.id
-        # puts frontend_facade.name
-        # puts frontend_facade.address
-        puts frontend_facade.expect_result(key)
-      end
+    # context "Get Property" do
+    #   it "dbfactory demo" ,:tag => 'Users2' do |example|
+    #     key = example.metadata[:tag]
+    #     frontend_facade = FrontendFacade.new(:ssh => 'Property_ssh', :db => 'Property_db')
+    #     # puts frontend_facade.id
+    #     # puts frontend_facade.name
+    #     # puts frontend_facade.address
+    #     puts frontend_facade.expect_result(key)
+    #   end
+    # end
   end
 end
