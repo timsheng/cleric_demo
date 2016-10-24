1014357b7774b2e9c7264289ef51b2f209928f13
dolores.zhang <dolores.zhang@student.com>
2016-10-24 11:07:57 +0800

    New user send message to create an enquiry on chatbot

--- api/wechat.rb
+++ api/wechat.rb
@@ -5,7 +5,8 @@ class Wechat < API
   base_uri http('base_uri')
 
   # just for example "how to use method in accessors"
-  row :user, :table => 'lead'
+  row :lead, :table => 'lead'
+  row :session, :table => 'session'
 
   def send_text_message payload
     self.class.post('', :body => payload)
